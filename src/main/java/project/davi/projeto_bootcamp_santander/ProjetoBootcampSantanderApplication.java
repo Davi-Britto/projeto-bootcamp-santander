0acc5a8a78b671605b52cbdcae5fc64a3a1f518f
Davi-Britto <davi.britto@sptech.school>
2024-07-26 13:56:39 -0300

    Initial commit

---
+++ src/main/java/project/davi/projeto_bootcamp_santander/ProjetoBootcampSantanderApplication.java
@@ -0,0 +1,13 @@
+package project.davi.projeto_bootcamp_santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBootcampSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBootcampSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/davi/projeto_bootcamp_santander/ProjetoBootcampSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package project.davi.projeto_bootcamp_santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBootcampSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
